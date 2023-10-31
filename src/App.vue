7ee7fa4b5986805305831bb463e359503adede82
on22catrinkienzle <118989508+TheresiaCat@users.noreply.github.com>
2023-10-31 14:17:31 +0100

    API Key and Connection

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'App', 
+  
+  //Gibt Objekt zurück mit den Daten von der API
+  data () {
+    return {
+      api_key: '87ecc09b44dd66f2699a03e91094bc23',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  }
 }
 </script>
 
